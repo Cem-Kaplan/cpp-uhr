34fa45040b020567cd210a9563463b209e990de3
Cem-Kaplan <cemk46908@gmail.com>
2025-12-07 09:11:06 +0100

    test commit

---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include <wx/wx.h>
+#include <chrono>
+#include <iostream>
+
+class App1 : public wxApp
+{
+public:
+    bool OnInit() override;
+};
+
+class Frame1 : public wxFrame
+{
+public:
+    Frame1(const wxString& title);
+
+private:
+    void HelloWorld(wxCommandEvent& event);
+};
+
+wxIMPLEMENT_APP(App1);
+
+bool App1::OnInit()
+{
+    Frame1* frame = new Frame1("Uhrzeit");
+    frame->Show();
+    return true;
+}
+
+Frame1::Frame1(const wxString& title) : wxFrame(nullptr, wxID_ANY, title) {
+    wxPanel* panel = new wxPanel(this);
+    wxButton* aktuallisiere_zeit = new wxButton(panel, wxID_ANY, "Aktuallisiere Zeit");
+    wxStaticText* aktuelle_zeit = new wxStaticText(panel, wxID_ANY, "Uhrzeit", wxPoint(20, 100));
+
+    aktuallisiere_zeit->Bind(wxEVT_BUTTON, &Frame1::HelloWorld, this);
+}
+
+void Frame1::HelloWorld(wxCommandEvent& event)
+{
+    std::cout << "HI" << std::endl;
+}

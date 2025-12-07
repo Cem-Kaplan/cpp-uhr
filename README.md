4bfd0c73c5709106715e197d728429d1d11f7c62
Cem-Kaplan <cemk46908@gmail.com>
2025-12-07 09:41:39 +0100

    finalisiert

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # cpp-uhr
+
+Hier habe ich eine Uhr mit wxWidgets in c++ gemacht.
+Erforderliche Bibliotheken:
+    - wxWidgets (wx.h)
--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <wx/wx.h>
 #include <chrono>
 #include <iostream>
+#include <format>
 
 class App1 : public wxApp
 {
@@ -14,7 +15,8 @@ public:
     Frame1(const wxString& title);
 
 private:
-    void HelloWorld(wxCommandEvent& event);
+    void HelloWorld(wxCommandEvent& event);    
+    wxStaticText* aktuelle_zeit;
 };
 
 wxIMPLEMENT_APP(App1);
@@ -26,15 +28,24 @@ bool App1::OnInit()
     return true;
 }
 
-Frame1::Frame1(const wxString& title) : wxFrame(nullptr, wxID_ANY, title) {
+Frame1::Frame1(const wxString& title) : wxFrame(nullptr, wxID_ANY, title), aktuelle_zeit(nullptr) {
     wxPanel* panel = new wxPanel(this);
     wxButton* aktuallisiere_zeit = new wxButton(panel, wxID_ANY, "Aktuallisiere Zeit");
-    wxStaticText* aktuelle_zeit = new wxStaticText(panel, wxID_ANY, "Uhrzeit", wxPoint(20, 100));
+    aktuelle_zeit = new wxStaticText(panel, wxID_ANY, "Uhr", wxPoint(20, 100));
 
     aktuallisiere_zeit->Bind(wxEVT_BUTTON, &Frame1::HelloWorld, this);
 }
 
 void Frame1::HelloWorld(wxCommandEvent& event)
 {
-    std::cout << "HI" << std::endl;
+    auto now = std::chrono::system_clock::now();
+    std::time_t t = std::chrono::system_clock::to_time_t(now);
+    std::cout << "Uhrzeit: " << t << std::endl;
+
+    // das ist der einzigste teil wo ich pures AI code nutzte, der rest ist selbst gemacht
+    wxDateTime dt(t);
+    wxString wxTime = dt.FormatISOCombined(' ');
+    aktuelle_zeit->SetLabel(wxTime);
+
+    aktuelle_zeit->SetLabel(wxTime);
 }
